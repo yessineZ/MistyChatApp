a6fb8c961f68f638586b65cd83184e609966062f
Yessine Zouari <145301341+yessineZ@users.noreply.github.com>
2024-07-17 06:31:13 +0100

    Update Messages.jsx

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect , useRef} from 'react'
 import Message from './Message'
 import useGetMessages from '../../hooks/useGetMessages'
 import MessageSkeleton from '../../../skeletons/MessageSkeleton';
@@ -6,7 +6,7 @@ import useListenMessages from '../../hooks/useListenMessages';
 
 const Messages = () => {
    let {loading , messages} = useGetMessages() ;
-
+  let lastMessage =useRef() ; 
   useListenMessages() ; 
 
   useEffect(() => {
@@ -15,9 +15,19 @@ const Messages = () => {
     }
   },)
 
+  setTimeout(() => {
+    lastMessage.current?.scrollIntoView({ behavior: 'smooth' }) ;  // scroll to the bottom of the messages container when new messages arrive
+  },50);
+
   return (
     <div className='px-4 flex-1 overflow-auto'>
-       {!loading && messages && messages.map(msg => <Message key={msg._id} message={msg} />)}
+       {!loading &&
+				messages.length > 0 &&
+				messages.map((message) => (
+					<div key={message._id} ref={lastMessage}>
+						<Message message={message} />
+					</div>
+				))}
        {loading && [...Array(3)].map((_,idx) =><MessageSkeleton key={idx} />)} 
       {!loading && !messages && (
         <p className='text-center text-gray-300'>Send a message to start the conversation...</p>
