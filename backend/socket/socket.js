2cc3e494a27e20cbb2b42801bafbdf67a078549f
Yessine Zouari <145301341+yessineZ@users.noreply.github.com>
2024-07-17 05:20:21 +0100

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ import express from 'express' ;
 export const server = http.createServer(app) ; 
  export const io = new Server(server,{
     cors: {
-        origin: '*',
+        origin: ["http://localhost:3000"],
         methods: ['GET', 'POST']
     }
  });
